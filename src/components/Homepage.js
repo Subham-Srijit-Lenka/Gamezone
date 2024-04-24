c723b3c2e05c25e2e9be7d10942a258bd0a4a1e8
Subham srijit Lenka <subhamsrijit19@gmail.com>
2024-04-24 16:24:12 +0530

    images added to public

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,18 +2,13 @@ import React from 'react'
 import Cards from './others/cards'
 import "./guessnumber/styles.css"
 
-import img1 from '/Users/srijit/Documents/GameZone/gamezone/src/components/image/tictactoe.jpg'
-import img2 from '/Users/srijit/Documents/GameZone/gamezone/src/components/image/snake.jpg'
-import img3 from '/Users/srijit/Documents/GameZone/gamezone/src/components/image/guessthenumber.jpg'
-
-
-
 export default function HomePage() {
   return (
     <div className='allCardContainer'>
-        <Cards title = "Tic-Tac-Toe" visit = "./tictactoe" imgURL = {img1} message="Play tic tac toe with friends" />
-        <Cards title = "Snake Game" visit = "./snakegame" imgURL= {img2} message="Try to score more..."/>
-        <Cards title = "Guess The Number" visit = "./guessthenumber" imgURL= {img3} message="Guess the correct number and Win" />
+        <Cards title = "Tic-Tac-Toe" visit = "./tictactoe" imgURL = './image/tictactoe.jpg' message="Play tic tac toe with friends" />
+        <Cards title = "Snake Game" visit = "./snakegame" imgURL= './image/snake.jpg' message="Try to score more..."/>
+        <Cards title = "Guess The Number" visit = "./guessthenumber" imgURL= './image/guessthenumber.jpg' message="Guess the correct number and Win" />
     </div>
   )
 }
+
